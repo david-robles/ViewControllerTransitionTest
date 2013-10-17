1cabc66c5b5cc7d7ee59adcba353bcb1da70fa1d
David Robles <master-nevi@users.noreply.github.com>
2013-10-17 15:03:28 -0700

    Initial Commit

---
+++ ViewControllerTransitionTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViewControllerTransitionTest
+//
+//  Created by David Robles on 10/17/13.
+//  Copyright (c) 2013 955 Dreams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewControllerTransitionTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViewControllerTransitionTest
+//
+//  Created by David Robles on 10/17/13.
+//  Copyright (c) 2013 955 Dreams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
